f9339aaba70d9da8cf32d48e7e2610f8e89546f7
Yichen <Yichen@mac>
2019-12-20 20:00:28 -0600

    add report, add java sample

---
+++ index.js
@@ -0,0 +1,165 @@
+let width = 954;
+let radius = width / 2;
+let colorNone = "#ccc";
+let colorOut = "#f00";
+let colorIn = "#00f";
+let test = "";
+
+let tree = d3.cluster().size([2 * Math.PI, radius - 100]);
+
+let line = d3.lineRadial()
+    .curve(d3.curveBundle.beta(0.85))
+    .radius(d => d.y)
+    .angle(d => d.x);
+
+
+function id(node) {
+    return `${node.parent ? id(node.parent) + "." : ""}${node.data.name}`;
+}
+
+function hierarchy(data, delimiter = ".") {
+    let root;
+    const map = new Map;
+    data.forEach(function find(data) {
+        console.log(data);
+        const {name} = data;
+        if (map.has(name)) return map.get(name);
+        const i = name.lastIndexOf(delimiter);
+        console.log( name.substring(0, i));
+        map.set(name, data);
+        if (i >= 0) {
+            find({name: name.substring(0, i), children: []}).children.push(data);
+            data.name = name.substring(i + 1);
+        } else {
+            root = data;
+        }
+        return data;
+    });
+    console.log(map);
+    return root;
+}
+
+function bilink(root) {
+    const map = new Map(root.leaves().map(d => [id(d), d]));
+    for (const d of root.leaves()) d.incoming = [], d.outgoing = d.data.imports.map(i => [d, map.get(i)]);
+    for (const d of root.leaves()) for (const o of d.outgoing) o[1].incoming.push(o);
+    return root;
+}
+
+function asyncGetFile(url) {
+    console.log("Getting text file");
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+        console.log("Made promise");
+    });
+}
+
+function main() {
+    document.getElementById('input-file')
+        .addEventListener('change', getFile);
+}
+
+
+function generateTree(data) {
+    const root = tree(bilink(d3.hierarchy(data)
+        .sort((a, b) => d3.ascending(a.height, b.height) || d3.ascending(a.data.name, b.data.name))));
+
+    d3.select("body").select("svg").remove();
+
+    const svg = d3.select("body").append("svg")
+        .attr("viewBox", [-width / 2, -width / 2, width, width]);
+
+    const node = svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll("g")
+        .data(root.leaves())
+        .join("g")
+        .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
+        .append("text")
+        .attr("dy", "0.31em")
+        .attr("x", d => d.x < Math.PI ? 6 : -6)
+        .attr("text-anchor", d => d.x < Math.PI ? "start" : "end")
+        .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
+        .text(d => d.data.name)
+        .each(function (d) {
+            d.text = this;
+        })
+        .on("mouseover", overed)
+        .on("mouseout", outed)
+        .call(text => text.append("title").text(d => `${id(d)}
+${d.outgoing.length} outgoing
+${d.incoming.length} incoming`));
+
+    const link = svg.append("g")
+        .attr("stroke", colorNone)
+        .attr("fill", "none")
+        .selectAll("path")
+        .data(root.leaves().flatMap(leaf => leaf.outgoing))
+        .join("path")
+        .style("mix-blend-mode", "multiply")
+        .attr("d", ([i, o]) => line(i.path(o)))
+        .each(function (d) {
+            d.path = this;
+        });
+
+    function overed(d) {
+        link.style("mix-blend-mode", null);
+        d3.select(this).attr("font-weight", "bold");
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", colorIn).raise();
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", colorIn).attr("font-weight", "bold");
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", colorOut).raise();
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", colorOut).attr("font-weight", "bold");
+    }
+
+    function outed(d) {
+        link.style("mix-blend-mode", "multiply");
+        d3.select(this).attr("font-weight", null);
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", null).attr("font-weight", null);
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", null).attr("font-weight", null);
+    }
+}
+
+function getFile(event) {
+    const input = event.target;
+    if ('files' in input && input.files.length > 0) {
+        placeFileContent(
+            document.getElementById('content-target'),
+            input.files[0]);
+    }
+    console.log(test);
+}
+
+function placeFileContent(target, file) {
+    readFileContent(file).then(content => {
+        target.value = content;
+        console.log(content);
+        test = content;
+
+        let myPromise = asyncGetFile('untitled.json');
+
+        myPromise.then((retrievedText) => {
+            let data = hierarchy(JSON.parse(content));
+            generateTree(data);
+        }).catch(
+            (reason) => {
+                console.log('Handle rejected promise (' + reason + ') here.');
+            });
+
+    }).catch(error => console.log(error));
+}
+
+function readFileContent(file) {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = error => reject(error);
+        reader.readAsText(file);
+    })
+}
